b9e3ae20dfbe3d3564a6d3ef0b4ac07a36f670fc
root <root@ip-172-31-21-222.ec2.internal>
2022-10-03 11:20:10 +0000

    Docker Install

--- 01-Docker/04-Docker-Compose/install-docker-compose.sh
+++ 01-Docker/04-Docker-Compose/install-docker-compose.sh
@@ -3,5 +3,5 @@
 DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
 mkdir -p $DOCKER_CONFIG/cli-plugins
 curl -SL https://github.com/docker/compose/releases/download/v2.11.2/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
-chmod +x .docker/cli-plugins/docker-compose
+chmod +x /root/.docker/cli-plugins/docker-compose
 docker compose version
